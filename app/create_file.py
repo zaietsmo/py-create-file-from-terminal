889795168ce64acf7e1fa6adeb35c9ebca233bbf
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-10-23 20:21:23 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -3,13 +3,13 @@ import os
 from datetime import datetime
 
 
-def create_directory(path: str) -> None:
-    try:
-        os.makedirs(path, exist_ok=True)
-        print(f"Directory '{path}' created!")
-    except OSError as e:
-        print(f"Error creating directory: {e}")
-        sys.exit(1)
+def create_directory(path_parts: list[str]) -> None:
+    if not os.path.exists(os.path.join(*path_parts)):
+        os.makedirs(os.path.join(*path_parts))
+        print(f"Directory '{os.path.join(*path_parts)}' created!")
+
+    else:
+        print(f"Directory '{os.path.join(*path_parts)}' already exists.")
 
 
 def create_file(file_path: str) -> None:
@@ -23,41 +23,32 @@ def create_file(file_path: str) -> None:
     with open(file_path, "a") as f:
         f.write(f"\n{timestamp}\n")
 
-        count = 1
+        str_count = 1
         while True:
-            content = input(f"Enter content line {count}: ")
+            content = input(f"Enter content line {str_count}: ")
             if content.lower() == "stop":
                 break
-            f.write(f"{count} {content}\n")
-            count += 1
-
-
-def main() -> None:
-    args = sys.argv[1:]
-
-    if "-d" in args:
-        d_index = args.index("-d")
-        dir_parts = args[d_index + 1:]
-        if "-f" in dir_parts:
-            f_index = dir_parts.index("-f")
-            dir_parts = dir_parts[:f_index]
-        directory_path = os.path.join(*dir_parts)
-        create_directory(directory_path)
-
-    if "-f" in args:
-        f_index = args.index("-f")
-        file_name = args[f_index + 1]
-        if "-d" in args:
-            directory_path = os.path.join(*args[args.index("-d") + 1:f_index])
-            file_path = os.path.join(directory_path, file_name)
-        else:
-            file_path = file_name
-
-        create_file(file_path)
+            f.write(f"{str_count} {content}\n")
+            str_count += 1
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Arguments Error!")
-    else:
-        main()
+
+    if "-d" in sys.argv:
+        dir_parts = sys.argv[sys.argv.index("-d") + 1:]
+        if "-f" in dir_parts:
+            dir_parts = dir_parts[:dir_parts.index("-f")]
+        create_directory(dir_parts)
+
+    if "-f" in sys.argv:
+        file_name = sys.argv[sys.argv.index("-f") + 1]
+        if "-d" in sys.argv:
+            directory_path = os.path.join(*sys.argv[
+                sys.argv.index("-d") + 1:sys.argv.index("-f")])
+            file_path = os.path.join(directory_path, file_name)
+        else:
+            file_path = file_name
+
+        create_file(file_path)
